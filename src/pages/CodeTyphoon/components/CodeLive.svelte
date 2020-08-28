2594804f1c20cd1a94fd9e6f18e42b9ba8329dd4
gaconkzk <gaconkzk@gmail.com>
2020-08-28 16:58:48 +0700

    I have event, and button, and console log

--- src/pages/CodeTyphoon/components/CodeLive.svelte
+++ src/pages/CodeTyphoon/components/CodeLive.svelte
@@ -1,12 +1,22 @@
 <script>
-  let current = ""
-  let isFocused = false
+  let current = "A quick brown fox jumps over the lazy dog"
+  let typed = ""
+  let isStarted = false
 
   const handleKeydown = (e) => {
-    console.log('down down', e)
+    if (isStarted) {
+      console.log(e)
+      e.preventDefault()
+    }
   }
 
-  const handleFocus = (e) => isFocused = true
+  const handleStart = () => isStarted = !isStarted
+
+  const render = (current) => {
+    
+
+    return current
+  }
 </script>
 
 <style lang="scss">
@@ -23,5 +33,7 @@
 <svelte:window on:keydown={handleKeydown} />
 
 <div id="codelive" class="rounded border-2 border-green-200 shadow-5 my-10">
-  <strong>A quick b<mark>r</mark>own <mark>f</mark>ox</strong> jumps over the lazy dog
+  {render(current)}
 </div>
+
+<button class="bg-green-500 rounded-lg w-24 h-12 text-white text-2xl antialiased font-bold shadow-2xl" on:click={handleStart}>{isStarted ? 'Stop' : 'Start'}</button>
