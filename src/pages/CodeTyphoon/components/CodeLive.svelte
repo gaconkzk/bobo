dbf8e34f5f12ab4cb5e2de81cd8898f5abd10de4
gaconkzk <gaconkzk@gmail.com>
2020-09-01 17:30:49 +0700

    fixed :bug: type more than necessary

--- src/pages/CodeTyphoon/components/CodeLive.svelte
+++ src/pages/CodeTyphoon/components/CodeLive.svelte
@@ -1,25 +1,54 @@
 <script>
+  import { createEventDispatcher } from 'svelte'
+
+  const dispatch = createEventDispatcher()
+
   import Keyboard from './Keyboard'
 
-  let current = "A quick brown fox jumps over the lazy dog"
+  // TODO current should be external prop and load by a mechanism (user allow to chose any thing they want to challenge or practice)
+  // future welcome meee yo yo yo
+  let current = `A quick brown fox jumps over the lazy dog.
+  - Fixed a bug: typed more than it should be`
+
   let all_words = current.replace(/<\/?[^>]+(>|$)/g, "").split(' ')
   let typed = ''
   let isStarted = false
 
   let codelive
 
-  const handleKeydown = (e) => {
-    if (isStarted) {
-      let possible_char_regex = /^[`~!@#$%^&*()-_=+[{\]}\\|;:'",<.>/?a-z0-9A-Z\s]$/
-      
+  const PressedState = {
+    NORMALKEY: 1,
+    BACKSPACE: 2,
+  }
+
+  const checkKey = (e) => {
+    let possible_char_regex = /^[`~!@#$%^&*()-_=+[{\]}\\|;:'",<.>/?a-z0-9A-Z\s]$/
       if (possible_char_regex.test(e.key)) {
-        typed += e.key
-        render()
+        return PressedState.NORMALKEY
       } else {
         if (e.key === 'Backspace') {
-          typed = typed.slice(0, -1)
-          render()
+          return PressedState.BACKSPACE
         }
+      }
+
+    return 0
+  }
+
+  const handleKeydown = (e) => {
+    if (isStarted) {
+      let shouldRender = checkKey(e)
+      switch (checkKey(e)) {
+        case PressedState.NORMALKEY:
+          typed += e.key
+          break
+        case PressedState.BACKSPACE:
+          typed = typed.slice(0, -1)
+          break
+        default:
+      }
+
+      if (shouldRender) {
+        render()
       }
 
       // TODO should stop this when ???
@@ -35,7 +64,7 @@
     if (isStarted) {
       typed = ''
       all_words = current.replace(/<\/?[^>]+(>|$)/g, '').split(' ')
-      codelive.innerHTML = all_words.join(' ')
+      codelive.innerHTML = all_words.join(' ').replace('\n', '<br/>')
     }
   }
 
@@ -45,14 +74,23 @@
       .filter(x => x >= 0)
   }
 
-  function htmlWord(idx, typed_words, all_words, eow = false) {
+  function htmlWord(idx, typed_words, eow = false) {
     let current_word = typed_words[idx]
-    let orig_word = all_words[idx].replace(/<\/?[^>]+(>|$)/g, "")
+    // use the original one, since we might changed some words
+    let orig_all = current.replace(/<\/?[^>]+(>|$)/g, "").split(' ')
+    let orig_word = orig_all[idx].replace(/<\/?[^>]+(>|$)/g, '')
     let gidx = compare_word(current_word, orig_word)
 
     let ca = orig_word.split('')
+    let cb = current_word.split('')
+    
     gidx.forEach(e_idx => {
-      ca[e_idx] = `<mark>${ca[e_idx]}</mark>`
+      if (ca[e_idx]) {
+        ca[e_idx] = `<mark>${ca[e_idx]}</mark>`
+      } else {
+        // no ca - the guy typed too much
+        ca[e_idx] = `<mark>${cb[e_idx]}</mark>`
+      }
     })
     
     ca.forEach((char, i) => {
@@ -78,13 +116,13 @@
 
     let current_word = typed_words[last_w_idx]
 
-    all_words[last_w_idx] = htmlWord(last_w_idx, typed_words, all_words)
+    all_words[last_w_idx] = htmlWord(last_w_idx, typed_words)
 
     if (!current_word.length && prev_w_idx >= 0) {
-      all_words[prev_w_idx] = htmlWord(prev_w_idx, typed_words, all_words, true)
+      all_words[prev_w_idx] = htmlWord(prev_w_idx, typed_words, true)
     }
 
-    let html = all_words.join(' ')
+    let html = all_words.join(' ').replace('\n', '<br/>')
     codelive.innerHTML = html
   }
 </script>
@@ -102,7 +140,7 @@
 
 <svelte:window on:keydown={handleKeydown} />
 
-<div id="codelive" class="rounded border-2 border-green-200 shadow-5 my-10" bind:this={codelive}>{current}</div>
+<div id="codelive" class="rounded border-2 border-green-200 shadow-5 my-10" bind:this={codelive}>{@html current.replace('\n', '<br/>')}</div>
 
 <button class="bg-green-500 rounded-lg w-24 h-12 text-white text-2xl antialiased font-bold shadow-2xl" on:click={handleStart}>{isStarted ? 'Stop' : 'Start'}</button>
 
