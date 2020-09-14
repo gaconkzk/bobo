18c18efcad08a26f56d49304187be1eb22a408d4
gaconkzk <gaconkzk@gmail.com>
2020-09-14 17:20:58 +0700

    use action env key

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 var firebaseConfig = {
-  apiKey: process.env.API_KEY || __bobo.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN || __bobo.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL || __bobo.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID || __bobo.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET || __bobo.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID || __bobo.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID || __bobo.env.APP_ID,
+  apiKey: __bobo.env.API_KEY,
+  authDomain: __bobo.env.AUTH_DOMAIN,
+  databaseURL: __bobo.env.DATABASE_URL,
+  projectId: __bobo.env.PROJECT_ID,
+  storageBucket: __bobo.env.STORAGE_BUCKET,
+  messagingSenderId: __bobo.env.MESSAGING_SENDER_ID,
+  appId: __bobo.env.APP_ID,
 }
 
 firebase.initializeApp(firebaseConfig)
