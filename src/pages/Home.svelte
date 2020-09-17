b8b3f2bd12390cb917492c85065d506b9115d248
gaconkzk <gaconkzk@gmail.com>
2020-09-17 16:55:54 +0700

    remove comments

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -23,25 +23,6 @@
   })
 </script>
 
-<!-- <nav class="flex items-center justify-between flex-wrap bg-blue-500 p-6">
-  <div class="flex items-center flex-shrink-0 text-white mr-6">
-    <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http_//www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-    <span class="font-semibold text-xl tracking-tight">Bobo</span>
-  </div>
-  <div class="block lg_hidden">
-    <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover_text-white hover_border-white">
-      <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http_//www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-    </button>
-  </div>
-  <div class="w-full block flex-grow lg_flex lg_items-center lg_w-auto">
-    <div class="text-sm lg_flex-grow">
-      <a href="/ct" class="block mt-4 lg_inline-block lg_mt-0 text-teal-200 hover_text-white mr-4">
-        Code Typhoon
-      </a>
-    </div>
-  </div>
-</nav> -->
-
 <style lang="scss">
   #homepage {
     font-family: 'Source Sans Pro', sans-serif;
