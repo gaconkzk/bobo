5aa43820cc3e771e8e2d373928ca4fca84fe2dff
gaconkzk <gaconkzk@gmail.com>
2020-09-21 15:38:41 +0700

    update maxdata per cat

--- src/pages/CodeTyphoon/Typhoons.svelte
+++ src/pages/CodeTyphoon/Typhoons.svelte
@@ -9,9 +9,9 @@
   // should be top 3
   let trend = [1,2,3]
   // should be top 10
-  let fighting = [1, 2, 3, 4, 5]
+  let fighting = [1, 2, 3, 4]
   // should be top 10
-  let data = [1,2,3,4,5,6,7,8,9]
+  let data = [1,2,3,4,5,6,7,8]
 </script>
 
 <style lang="scss">
@@ -23,7 +23,7 @@
 <section>
   <h3 class="cat">Hot trending</h3>
 
-  <div class="flex px-2 items-center">
+  <div class="flex px-2 items-center grid grid-flow-row sm_grid-cols-1 md_grid-cols-2 lg_grid-cols-3">
     {#each trend as item}
       <Card data={item}/>
     {/each}
@@ -33,7 +33,7 @@
 <section>
   <h3 class="cat">Fighting</h3>
 
-  <div class="grid grid-flow-row md_grid-cols-4 lg_grid-cols-4 gap-4 sm_grid-cols-2 px-2">
+  <div class="grid grid-flow-row md_grid-cols-3 lg_grid-cols-4 gap-4 sm_grid-cols-1 px-2">
     {#each fighting as item}
       <Card data={item}/>
     {/each}
@@ -43,7 +43,7 @@
 <section>
   <h3 class="cat">Practice</h3>
 
-  <div class="grid grid-flow-row md_grid-cols-4 lg_grid-cols-4 gap-4 sm_grid-cols-2 px-2">
+  <div class="grid grid-flow-row md_grid-cols-3 lg_grid-cols-4 gap-4 sm_grid-cols-1 px-2">
     {#each data as item}
       <Card data={item}/>
     {/each}
