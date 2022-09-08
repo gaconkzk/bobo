8cbabb19df22c236c4a836956741831f22d38603
gaconkzk <gaconkzk@gmail.com>
2022-09-08 07:46:40 +0700

    change to pixi

--- src/pages/Phaser/index.svelte
+++ src/pages/Phaser/index.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
   import { CharacterEnum } from '$components/Character'
   import Character from '$components/Character.svelte'
 
@@ -7,6 +8,11 @@
   import Background from './components/Background.svelte'
   import Brain from './components/Brain.svelte'
   import Cat from './components/Cat.svelte'
+
+  let phaserContainer
+
+  let booting = false
+  onMount(() => {})
 </script>
 
 <div>
@@ -14,16 +20,20 @@
     <Character model={CharacterEnum.CAT} direction="right" />
     <Back2HomeBtn />
   </div>
-  <div id="testingPhaser">
-    <Game parent="testingPhaser" backgroundColor={0xffffff}>
-      <Scene
-        key="testScene"
-        preload={() => ({ key: 'splash', url: 'imgs/phaser4.png' })}
-      >
-        <!-- <Background key="background" /> -->
-        <Brain />
-        <Cat />
-      </Scene>
-    </Game>
+  <div id="testingPhaser" bind:this={phaserContainer}>
+    {#if booting}
+      <span>Waiting</span>
+    {:else}
+      <Game parent="testingPhaser" backgroundColor={0xffffff}>
+        <Scene
+          key="testScene"
+          preload={() => ({ key: 'splash', url: 'imgs/phaser4.png' })}
+        >
+          <!-- <Background key="background" /> -->
+          <Brain />
+          <Cat />
+        </Scene>
+      </Game>
+    {/if}
   </div>
 </div>
