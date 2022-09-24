b1c4ecd261e3f3e9af7f4bb214a5d566023b244b
gaconkzk <gaconkzk@gmail.com>
2022-09-24 12:14:52 +0700

    use pixijs 7.0.0 alpha since loader removed is big change

--- src/components/PIXI/Assets.svelte
+++ src/components/PIXI/Assets.svelte
@@ -1,148 +1,86 @@
 <script context="module" lang="ts">
-  interface AssetsContext<T extends AssetsClass> {
+  interface AssetsContext<T extends PIXI.AssetsClass> {
     assets?: T
   }
 
-  export function getAssets<T extends AssetsClass>() {
-    return getContext<AssetsContext<T>>('pixi/assets') ?? {}
+  export function getAssets<T extends PIXI.AssetsClass>() {
+    return (
+      getContext<AssetsContext<T>>('pixi/assets') ?? { assets: PIXI.Assets }
+    )
   }
 
   export function getResource<T = any>(name: string): T {
     const { assets } = getAssets()
+
     if (!assets) {
       throw new Error('getResource requires a parent <Assets /> component')
     }
 
-    return assets.get(name) as any as T
+    const res = assets.get<T>(name)
+    return res as any as T
   }
 </script>
 
 <script lang="ts">
   import * as PIXI from 'pixi.js'
-  import { Assets, AssetsClass } from '@pixi/assets'
-
-  import {
-    createEventDispatcher,
-    setContext,
-    getContext,
-    onMount,
-  } from 'svelte'
-  import { getRenderer } from './Renderer.svelte'
+  import { onMount, getContext, setContext } from 'svelte'
 
   interface $$Slots {
     default: {
       progress?: number
     }
     loading: {
-      progress: number
+      progress?: number
     }
   }
 
-  interface $$Events {
-    complete: CustomEvent<AssetsClass>
-    progress: CustomEvent<AssetsClass>
-    error: CustomEvent<AssetsClass>
-    start: CustomEvent<AssetsClass>
-    load: CustomEvent<AssetsClass>
-  }
+  type T = $$Generic<PIXI.AssetsClass>
 
-  type T = $$Generic<AssetsClass>
   type $$Props = {
     instance?: T
-    resources?: string[] | Array<[string, string]>
+    resources?:
+      | string[]
+      | Array<
+          [string | string[], string | (string | PIXI.ResolveAsset)[], unknown?]
+        >
   }
-  const dispatch = createEventDispatcher()
+
+  export let instance = PIXI.Assets as T
+
+  setContext<AssetsContext<T>>('pixi/assets', { assets: instance })
 
   /**
    * An array of urls or arguments to be passed into PIXI.Loader's add() function
-   * @type {string[] | Array<[string, string]>}
+   * @type {string[] | Array<[string, string, PIXI.IAddOptions, () => any]>}
    */
-  export let resources: string[] | Array<[string, string]> = []
+  export let resources:
+    | string[]
+    | Array<
+        [string | string[], string | (string | PIXI.ResolveAsset)[], unknown?]
+      > = []
 
-  /**
-   * The PIXI.Loader instance. Can be set or bound to.
-   *
-   * @type {PIXI.Assets}
-   */
-  export let instance: T = new AssetsClass() as T
-
-  setContext<AssetsContext<T>>('pixi/loader', { assets: instance })
-
-  const { invalidate } = getRenderer()
-
-  let progress = 0
-  let loading = resources.length > 0
-
+  let loading = true
   onMount(async () => {
     loading = true
-    await instance.init()
-    let prms = resources.map(async (url) => {
+    let keys = []
+    resources.forEach((url) => {
       if (Array.isArray(url)) {
-        console.log('imgload', ...url)
+        const [name, ...rest] = url
         // @ts-ignore
-        return await instance.add(url[0], url[1])
+        instance.add(name, ...rest)
+
+        keys.push(name)
       } else {
-        return await instance.load(url)
+        // instance.load(url)
       }
     })
-
-    try {
-      await Promise.all(prms)
-    } catch (err) {
-      console.error('error loading', err)
-    }
-
-    dispatch('complete')
-    invalidate()
+    await instance.load(keys)
     loading = false
-
-    // instance.load()
-
-    // function onComplete(ev) {
-    //   dispatch('complete', ev)
-    //   invalidate()
-    //   loading = false
-    // }
-
-    // function onProgress(ev) {
-    //   dispatch('progress', ev)
-    //   invalidate()
-    //   progress = ev.progress
-    // }
-
-    // function onError(ev) {
-    //   dispatch('error', ev)
-    //   invalidate()
-    // }
-
-    // function onStart(ev) {
-    //   dispatch('start', ev)
-    //   invalidate()
-    // }
-
-    // function onLoad(ev) {
-    //   dispatch('load', ev)
-    //   invalidate()
-    // }
-
-    // const onCompleteId = instance.onComplete.add(onComplete)
-    // const onProgressId = instance.onProgress.add(onProgress)
-    // const onErrorId = instance.onError.add(onError)
-    // const onStartId = instance.onStart.add(onStart)
-    // const onLoadId = instance.onLoad.add(onLoad)
-
-    return () => {
-      // instance.onComplete.detach(onCompleteId)
-      // instance.onProgress.detach(onProgressId)
-      // instance.onError.detach(onErrorId)
-      // instance.onStart.detach(onStartId)
-      // instance.onLoad.detach(onLoadId)
-    }
   })
 </script>
 
 {#if loading}
-  <slot name="loading" {progress} />
+  <slot name="loading" />
 {:else}
   <slot />
 {/if}
--- src/pages/PIXI/index.svelte
+++ src/pages/PIXI/index.svelte
@@ -1,24 +1,22 @@
 <script lang="ts">
-  import * as PIXI from 'pixi.js'
-
   import Application from '$components/PIXI/Application.svelte'
-  import Loader from '$components/PIXI/Loader.svelte'
+  import Assets, { getResource } from '$components/PIXI/Assets.svelte'
   import Text from '$components/PIXI/Text.svelte'
   import Sprite from '$components/PIXI/Sprite.svelte'
 </script>
 
 <Application width={400} height={400} antialias backgroundColor={0xffffff}>
-  <Loader let:progress resources={[['brain', '/imgs/brain.png']]}>
+  <Assets resources={[['brain', '/imgs/brain.png']]}>
     <slot slot="loading">
-      <Text text={`Loading... ${progress}`} x={200} y={200} anchor={0.5} />
+      <Text text={`Loading... `} x={200} y={200} anchor={0.5} />
     </slot>
-
+    <Text text={`hello... `} x={200} y={200} anchor={0.5} />
     <Sprite
-      texture={PIXI.Texture.from('brain')}
+      texture={getResource('brain')}
       x={20}
       y={50}
       width={360}
       height={300}
     />
-  </Loader>
+  </Assets>
 </Application>
